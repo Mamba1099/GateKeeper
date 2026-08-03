b623a99a97486b693e546cfce254a58960a4a77b
Mamba1099 <mambakaranja240@gmail.com>
2026-08-03 20:04:08 +0300

    feat: add deployment configuration for netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "prisma", "@prisma/client"];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
